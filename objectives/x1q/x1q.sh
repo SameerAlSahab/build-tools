a1366b0db782e50edf99eb7b0cea65241de6e3c7
jannatnehapets-droid <jannatnehapets@gmail.com>
2026-01-11 09:30:49 +0600

    x1q lets see if boot as of now then add fixes

---
+++ objectives/x1q/x1q.sh
@@ -0,0 +1,26 @@
+# Objective details
+MODEL_NAME="Galaxy S20 5G"
+CODENAME="x1q"
+VNDK="33"
+
+# The model using as source
+# r9q is less bloated and similar structure as x1q
+MODEL="SM-G990B"
+CSC="EUX" 
+IMEI="353718681234563"
+
+# Stock firmware
+STOCK_MODEL="SM-G981N"
+STOCK_CSC="KOO"
+STOCK_IMEI="355995110205095"
+
+# Extra fw if needed
+#EXTRA_MODEL=""
+#EXTRA_CSC=""
+#EXTRA_IMEI=""
+
+# External
+FILESYSTEM=ext4
+# TODO: Add erofs on ramdisk beside kernel
+#FILESYSTEM="erofs"
+
